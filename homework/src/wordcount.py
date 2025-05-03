b6782a0fe5df678d8170ed6fa27fa95821c0746f
lfsotoc <lfsotoc@unal.edu.co>
2025-05-03 09:55:24 -0500

    foto1

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -2,14 +2,14 @@
 import os
 
 
-# def read_all_lines():
-# all_lines = []
-# input_file_list = os.listdir("data/input/")
-# for filename in input_file_list:
-# with open(filename, "r", encoding="utf-8") as f:
-# lines = f.readlines()
-# all_lines.extend(lines)
-# return all_lines
+def read_all_lines():
+    all_lines = []
+    input_file_list = os.listdir("data/input/")
+    for filename in input_file_list:
+        with open(filename, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+    return all_lines
 
 
 def main():
